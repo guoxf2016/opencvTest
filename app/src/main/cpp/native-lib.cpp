fd5ab4177498c023f56544de0af201fd95910f7a
guoxf2016 <guoxiaofei2016@gmail.com>
2019-11-08 19:06:37 +0800

    add opencv sample with camera

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,10 @@
 #include <jni.h>
 #include <string>
 
-#include <opencv/cv.h>
-#include <opencv2/opencv.hpp>
+//#include <opencv/cv.h>
+//#include <opencv2/opencv.hpp>
 
-using namespace cv;
+//using namespace cv;
 
 extern "C" JNIEXPORT jstring
 
@@ -12,7 +12,7 @@ JNICALL
 Java_com_example_guoxiaofei_opencvtest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    Mat m = imread("bike.jpg");
+//    Mat m = imread("bike.jpg");
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
